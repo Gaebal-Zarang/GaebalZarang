34fd9fa4faea1faff8322a3a28c80f5d8be042bb
asqw887 <asqw887@gmail.com>
2022-08-08 23:08:15 +0900

    feat: Textfiled padding extension #1

---
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/CustomNextButton.swift
@@ -0,0 +1,20 @@
+//
+//  CustomNextButton.swift
+//  iOS-Gaebalzarang
+//
+//  Created by 최예주 on 2022/08/08.
+//
+
+import UIKit
+
+class CustomNextButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
