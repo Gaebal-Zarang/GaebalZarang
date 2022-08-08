15832286ddfc980e17ab607b45ac9df8b7424347
wnsxor1993 <wnsxor1993@naver.com>
2022-08-08 16:13:42 +0900

    feat: LoginViewController 생성 #3

---
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  iOS-Gaebalzarang
+//
+//  Created by juntaek.oh on 2022/08/08.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
