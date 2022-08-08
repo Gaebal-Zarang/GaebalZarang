378a6c3e9354bbdeb7be801521ad1bac3b2f1f6b
wnsxor1993 <wnsxor1993@naver.com>
2022-08-08 11:27:04 +0900

    feat: xcode 프로젝트 설정

---
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  iOS-Gaebalzarang
+//
+//  Created by juntaek.oh on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
