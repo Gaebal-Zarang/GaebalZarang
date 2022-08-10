79c614432ee38423e401cfc2c4d24f64bd6ca40a
wnsxor1993 <wnsxor1993@naver.com>
2022-08-10 14:17:25 +0900

    feat: SignUpNameIDView 구현 및 SignUPViewController 적용 #4

--- iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/View/SignUpViewController.swift
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/View/SignUpViewController.swift
@@ -8,9 +8,30 @@
 import UIKit
 
 final class SignUpViewController: UIViewController {
-    
+
+    private lazy var nameIDView = SignUpNameIDView(with: view.frame)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        configureLayouts()
+    }
+}
+
+private extension SignUpViewController {
+
+    func configureLayouts() {
+        view.addSubviews(nameIDView)
+
+        let nameIDWidth = DesignGuide.estimateXAxisLength(origin: 322, frame: view.frame)
+        let nameIDHeight = DesignGuide.estimateYAxisLength(origin: 146, frame: view.frame)
+        let nameIDTopConstant = DesignGuide.estimateYAxisLength(origin: 21, frame: view.frame)
+
+        NSLayoutConstraint.activate([
+            nameIDView.widthAnchor.constraint(equalToConstant: nameIDWidth),
+            nameIDView.heightAnchor.constraint(equalToConstant: nameIDHeight),
+            nameIDView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: nameIDTopConstant),
+            nameIDView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
     }
 }
