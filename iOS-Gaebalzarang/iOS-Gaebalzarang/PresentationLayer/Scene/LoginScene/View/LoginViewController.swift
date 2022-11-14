18f443561b614e8471356908e0e41f49a8043120
Zeto <>
2022-11-14 16:54:46 +0900

    refactor: LoginVC 레이아웃 수정

--- iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/LoginScene/View/LoginViewController.swift
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/LoginScene/View/LoginViewController.swift
@@ -8,157 +8,158 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import Then
+import SnapKit
 
 // TODO: 텍스트 필드를 가진 모든 VC에서 키보드 올라오면 뷰도 같이 올라가고, 바깥을 터치하면 키보드 해제되는 기능 구현 필요
 class LoginViewController: UIViewController {
 
     let disposeBag = DisposeBag()
 
-    private lazy var logoView: UIImageView = {
-        let imageView = UIImageView()
-        let logoImage = UIImage(named: "logo")
-        imageView.image = logoImage
-        imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .gray
-        imageView.translatesAutoresizingMaskIntoConstraints = false
+    private let logoView = UIImageView().then {
+        $0.image = UIImage(named: "logo")
+        $0.contentMode = .scaleAspectFit
+        $0.backgroundColor = .gray
+    }
 
-        return imageView
-    }()
+    private let idPwStackView = UIStackView().then {
+        $0.spacing = 12
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+    }
 
-    private lazy var idTextField: CustomTextField = {
-        let textField = CustomTextField()
-        textField.placeholder = "ID"
-        textField.setPlaceHolder()
-        textField.translatesAutoresizingMaskIntoConstraints = false
+    private let showButton = UIButton().then {
+        $0.setTitle("Show", for: .normal)
+        $0.titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.setTitleColor(.gzGreen, for: .normal)
+    }
 
-        return textField
-    }()
+    private let searchStackView = UIStackView().then {
+        $0.spacing = 10
+        $0.axis = .horizontal
+        $0.alignment = .center
+        $0.distribution = .fill
+    }
 
-    private lazy var pswTextField: CustomTextField = {
-        let pswField = CustomTextField()
-        pswField.addRightPadding(with: 75)
-        pswField.placeholder = "PW"
-        pswField.setPlaceHolder()
-        pswField.translatesAutoresizingMaskIntoConstraints = false
+    private let loginButton = CustomWideButton(isEnabled: true).then {
+        $0.setTitle("로그인", for: .normal)
+    }
 
-        return pswField
-    }()
-
-    private lazy var showButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Show", for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 16, weight: .regular)
-        button.setTitleColor(.gzGreen, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-
-        return button
-    }()
-
-    private lazy var loginButton: CustomWideButton = {
-        let button = CustomWideButton(isEnabled: true)
-        button.setTitle("로그인", for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-
-        return button
-    }()
-
-    private lazy var searchSignView: LoginSearchSignView = {
-        let view = LoginSearchSignView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-
-        return view
-    }()
+    private var idPwTextFields = [CustomTextField]()
+    private var searchButtons = [UIButton]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         view.backgroundColor = .white
         configureLayouts()
-        configureCornerRound()
         configureInnerActionBinding()
+    }
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+
+        configureCornerRound()
     }
 }
 
 private extension LoginViewController {
 
     func configureLayouts() {
+        view.addSubviews(logoView, idPwStackView, showButton, searchStackView, loginButton)
+        self.configureStackSubViews()
 
-        view.addSubviews(logoView, idTextField, pswTextField, showButton, searchSignView, loginButton)
+        logoView.snp.makeConstraints { make in
+            make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).offset(111)
+            make.width.equalTo(150)
+            make.height.equalTo(68)
+            make.centerX.equalToSuperview()
+        }
 
-        let defaultHeight = DesignGuide.estimateYAxisLength(origin: 50, frame: view.frame)
+        idPwStackView.snp.makeConstraints { make in
+            make.top.equalTo(logoView.snp.bottom).offset(74)
+            make.leading.equalToSuperview().offset(26)
+            make.trailing.equalToSuperview().offset(-26)
+            make.height.equalTo(112)
+        }
 
-        let logoViewTopConstant = DesignGuide.estimateYAxisLength(origin: 45, frame: view.frame)
+        showButton.snp.makeConstraints { make in
+            make.centerY.equalTo(idPwStackView.subviews[1].snp.centerY)
+            make.trailing.equalTo(idPwStackView.snp.trailing).offset(-29)
+            make.width.equalTo(40)
+            make.height.equalTo(25)
+        }
 
-        NSLayoutConstraint.activate([
-            logoView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            logoView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: logoViewTopConstant)
-        ])
+        searchStackView.snp.makeConstraints { make in
+            make.top.equalTo(idPwStackView.snp.bottom).offset(12)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(19)
+        }
 
-        let idTextTopConstant = DesignGuide.estimateYAxisLength(origin: 68, frame: view.frame)
-        let idTextWidth = DesignGuide.estimateXAxisLength(origin: 322, frame: view.frame)
+        loginButton.snp.makeConstraints { make in
+            make.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.bottom).offset(-29)
+            make.width.equalTo(idPwStackView.snp.width)
+            make.centerX.equalToSuperview()
+            make.height.equalTo(50)
+        }
+    }
 
-        NSLayoutConstraint.activate([
-            idTextField.topAnchor.constraint(equalTo: logoView.bottomAnchor, constant: idTextTopConstant),
-            idTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            idTextField.widthAnchor.constraint(equalToConstant: idTextWidth),
-            idTextField.heightAnchor.constraint(greaterThanOrEqualToConstant: defaultHeight)
-        ])
+    func configureStackSubViews() {
+        for num in 0...1 {
+            let textField = CustomTextField().then {
+                if num == 0 {
+                    $0.placeholder = "ID"
 
-        let pswTextTopConstant = DesignGuide.estimateYAxisLength(origin: 12, frame: view.frame)
+                } else {
+                    $0.placeholder = "PW"
+                    $0.addRightPadding(with: 75)
+                }
 
-        NSLayoutConstraint.activate([
-            pswTextField.topAnchor.constraint(equalTo: idTextField.bottomAnchor, constant: pswTextTopConstant),
-            pswTextField.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            pswTextField.widthAnchor.constraint(equalToConstant: idTextWidth),
-            pswTextField.heightAnchor.constraint(greaterThanOrEqualToConstant: defaultHeight)
-        ])
+                $0.setPlaceHolder()
+                $0.sizeToFit()
+            }
 
-        let showBtnWidth = DesignGuide.estimateXAxisLength(origin: 40, frame: view.frame)
-        let showBtnHeight = DesignGuide.estimateYAxisLength(origin: 25, frame: view.frame)
-        let showBtnTrailingConstant = -(DesignGuide.estimateXAxisLength(origin: 56, frame: view.frame))
+            let button = UIButton().then {
+                if num == 0 {
+                    $0.setTitle("아이디 비밀번호 찾기", for: .normal)
 
-        NSLayoutConstraint.activate([
-            showButton.centerYAnchor.constraint(equalTo: pswTextField.centerYAnchor),
-            showButton.widthAnchor.constraint(equalToConstant: showBtnWidth),
-            showButton.heightAnchor.constraint(equalToConstant: showBtnHeight),
-            showButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: showBtnTrailingConstant)
-        ])
+                } else {
+                    $0.setTitle("회원가입", for: .normal)
+                }
 
-        let searchSignWidth = DesignGuide.estimateXAxisLength(origin: 235, frame: view.frame)
-        let searchSignHeight = DesignGuide.estimateYAxisLength(origin: 19, frame: view.frame)
+                $0.titleLabel?.textAlignment = .center
+                $0.titleLabel?.font = .systemFont(ofSize: 14, weight: .regular)
+                $0.sizeToFit()
+                $0.setTitleColor(.gzGray3, for: .normal)
+            }
 
-        NSLayoutConstraint.activate([
-            searchSignView.topAnchor.constraint(equalTo: pswTextField.bottomAnchor, constant: pswTextTopConstant),
-            searchSignView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            searchSignView.widthAnchor.constraint(equalToConstant: searchSignWidth),
-            searchSignView.heightAnchor.constraint(equalToConstant: searchSignHeight)
-        ])
+            idPwStackView.addArrangedSubview(textField)
+            idPwTextFields.append(textField)
 
-        let buttonBottomConstant = DesignGuide.estimateYAxisLength(origin: 24, frame: view.frame)
+            searchStackView.addArrangedSubview(button)
+            searchButtons.append(button)
+        }
 
-        NSLayoutConstraint.activate([
-            loginButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -(buttonBottomConstant)),
-            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            loginButton.widthAnchor.constraint(equalToConstant: idTextWidth),
-            loginButton.heightAnchor.constraint(greaterThanOrEqualToConstant: defaultHeight)
-        ])
+        let label = UILabel().then {
+            $0.text = "/"
+            $0.font = .systemFont(ofSize: 14, weight: .regular)
+            $0.textColor = .gzGray3
+            $0.textAlignment = .center
+            $0.sizeToFit()
+        }
+
+        searchStackView.insertArrangedSubview(label, at: 1)
     }
 
     func configureCornerRound() {
-        let viewRound = DesignGuide.estimateWideViewCornerRadius(frame: view.frame)
+        idPwTextFields.forEach {
+            $0.setCornerRound(value: ($0.frame.height / 2))
+        }
 
-        idTextField.setCornerRound(value: viewRound)
-        pswTextField.setCornerRound(value: viewRound)
-        loginButton.setCornerRound(value: viewRound)
+        loginButton.setCornerRound(value: (loginButton.frame.height / 2))
     }
 
     func configureInnerActionBinding() {
-        searchSignView.setSignUpAction()
-            .drive { [weak self] _ in
-                let nextVC = SignUpViewController()
-                let naviVC = UINavigationController(rootViewController: nextVC)
-                naviVC.modalPresentationStyle = .overFullScreen
-                self?.present(naviVC, animated: true)
-            }
-            .disposed(by: disposeBag)
+
     }
 }
