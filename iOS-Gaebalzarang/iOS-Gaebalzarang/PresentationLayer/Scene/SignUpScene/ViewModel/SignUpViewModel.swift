2f1baf8f8fad5bb9d274e05d61c3747c49c0b52b
Zeto <>
2022-11-16 14:53:01 +0900

    feat: SignUpVM - ValidityUsecase 외의 바인딩 구현

--- iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/ViewModel/SignUpViewModel.swift
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/ViewModel/SignUpViewModel.swift
@@ -33,17 +33,96 @@ final class SignUpViewModel: ViewModel {
         let isActiveConfirmIdButtonRelay = PublishRelay<Bool>()
     }
     
+    private let validateUsecase: CheckUsecase
     private let output = Output()
     private let disposeBag = DisposeBag()
     
     private var typedPswString = ""
 
-    init() {
-        
+    init(validateUsecase: CheckUsecase) {
+        self.validateUsecase = validateUsecase
     }
     
     func transform(input: Input) -> Output {
+        self.bindValidateObserver(with: input)
+        self.bindButtonObserver(with: input)
         
         return output
+    }
+}
+
+// MARK: Binding Input and Usecase's Output
+private extension SignUpViewModel {
+    
+    // 유효성 관련 바인딩
+    func bindValidateObserver(with input: Input) {
+        input.typedNameValue
+            .map { return ($0 != nil) ? $0! : "" }
+            .subscribe { [weak self] value in
+                guard let self = self else { return }
+                
+                let validation = self.validateUsecase.execute(with: value, classify: .name)
+                self.output.validNameRelay.accept(validation)
+            }
+            .disposed(by: disposeBag)
+        
+        input.typedIdValue
+            .map { return ($0 != nil) ? $0! : "" }
+            .subscribe { [weak self] value in
+                guard let self = self else { return }
+                
+                let validation = self.validateUsecase.execute(with: value, classify: .id)
+                self.output.validIdRelay.accept(validation)
+            }
+            .disposed(by: disposeBag)
+        
+        input.typedPwValue
+            .map { return ($0 != nil) ? $0! : "" }
+            .subscribe { [weak self] value in
+                guard let self = self else { return }
+                
+                let validation = self.validateUsecase.execute(with: value, classify: .psw)
+                self.output.validPwRelay.accept(validation)
+                
+                if let value = value.element {
+                    self.typedPswString = value
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        input.typedConfirmPwValue
+            .map { return ($0 != nil) ? $0! : "" }
+            .subscribe { [weak self] value in
+                guard let self = self else { return }
+                
+                let validation = (value == self.typedPswString)
+                self.output.validConfirmPwRelay.accept(validation)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    // 버튼과 관련된 바인딩
+    func bindButtonObserver(with input: Input) {
+        Observable
+            .combineLatest(output.validNameRelay.distinctUntilChanged(), output.validIdRelay.distinctUntilChanged(), output.validPwRelay.distinctUntilChanged(), output.validConfirmPwRelay.distinctUntilChanged(), resultSelector: { (name, id, psw, confirm) in
+            
+                return [name, id, psw, confirm]
+            })
+            .retry(3)
+            .map { return !($0.contains(false)) }
+            .bind(to: output.isEnableNextButtonRelay)
+            .disposed(by: disposeBag)
+        
+        output.isActiveConfirmIdButtonRelay
+            .subscribe { [weak self] _ in
+                // TODO: 아이디 중복 검사 로직 추가
+            }
+            .disposed(by: disposeBag)
+        
+        output.isActiveNextButtonRelay
+            .subscribe { [weak self] _ in
+                // TODO: 회원가입 진행 결과 로직 추가
+            }
+            .disposed(by: disposeBag)
     }
 }
