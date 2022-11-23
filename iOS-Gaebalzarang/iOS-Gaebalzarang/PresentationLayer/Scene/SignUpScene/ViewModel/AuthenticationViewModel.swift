594b11b340de9e364b4e38ffb759ee18815d1a35
Zeto <>
2022-11-23 15:39:52 +0900

    feat: AuthenticationVM Binding 구현

--- iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/ViewModel/AuthenticationViewModel.swift
+++ iOS-Gaebalzarang/iOS-Gaebalzarang/PresentationLayer/Scene/SignUpScene/ViewModel/AuthenticationViewModel.swift
@@ -27,6 +27,7 @@ final class AuthenticationViewModel: ViewModel {
     }
 
     private let output = Output()
+    private let disposeBag = DisposeBag()
 
     private var typedPhoneNumber: String?
     private var typedAuthenticNumber: String?
@@ -36,7 +37,35 @@ final class AuthenticationViewModel: ViewModel {
     }
 
     func transform(input: Input) -> Output {
-
+        input.typedPhoneValue
+            .drive { [weak self] phoneNum in
+                self?.typedPhoneNumber = phoneNum
+            }
+            .disposed(by: disposeBag)
+        
+        input.tapRequestAuthenticButton
+            .drive { [weak self] _ in
+                // TODO: 입력 전화번호 서버에 넘겨주는 통신 구현 관련 Usecase
+                
+                self?.output.isPushedPhoneNumberRelay.accept(true)
+            }
+            .disposed(by: disposeBag)
+        
+        input.typedAuthenticValue
+            .drive { [weak self] authenticValue in
+                self?.typedAuthenticNumber = authenticValue
+            }
+            .disposed(by: disposeBag)
+        
+        input.tapConfirmAuthenticButton
+            .drive { [weak self] _ in
+                // TODO: 입력 인증번호 서버에 넘겨주는 통신 구현 관련 Usecase
+                // 서버 response 값에 따라 Bool 값 변경하여 전달
+                self?.output.isCorrectAuthenticRelay.accept(true)
+                self?.output.canMoveToNextRelay.accept(true)
+            }
+            .disposed(by: disposeBag)
+        
         return output
     }
 }
